034de2c7f0c9cbcceff57f9e9e8a5652f803f1fb
Avinash <Avinashkumarsinha007@gmail.com>
2021-08-21 18:01:28 +0530

    assignment

---
+++ goodera_assignme/src/components/navbar.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styles from "./Navbar.module.css"
+
+
+export const Navbar = () => (
+    <>
+        <div className={styles.container_nav}>
+            <div className={styles.container_1}>
+            <div className={styles.container_2}>
+                <div className={styles.text1}>
+
+                    JobHunt
+                </div>
+                <div className={styles.text2}>
+                    Find Jobs
+                </div>
+                <div className={styles.text3}>
+                    Upskill Yourself
+                </div>
+            </div>
+            <div className={styles.container_3}>
+                <div className={styles.text4}>
+                    Post a Job
+                </div>
+                <div className={styles.text5}>
+                    Sign in
+                </div>
+                </div>
+            </div>
+            <div className={styles.container_4}>
+                <p className={styles.text6}>Find Your Dream Job</p>
+                <p className={styles.text7}>Browse through thousand of full-time or part-time jobs near you.</p>
+            </div>
+            <div className={styles.container_5}>
+                <div className={styles.container_6}>
+                    <div>
+
+                    </div>
+                    <div >
+                        <input className={styles.item8} type="text" placeholder="Job title or keyword"/>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div  className={styles.item9}>
+                        <select name="location" id="location">
+                            <option value="" >
+                                select location
+                            </option>
+                        </select>
+                    </div>
+                    <div  className={styles.button}>
+                        <p style={{marginTop:10}}>Search</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={styles.container_7} >
+            <div className={styles.container_9} >
+                <div className={styles.container_8}>
+                  
+                </div>
+                <div>
+                    <h3>
+                    Executive Producer
+                    </h3>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </div>
+            <div className={styles.container_9} >
+                <div className={styles.container_8}>
+                  
+                </div>
+                <div>
+                    <h3>
+                    Executive Producer
+                    </h3>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </>
+)
